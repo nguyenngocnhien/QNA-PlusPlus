129db86111363126835780e8543b9fcf09e0f26d
Nguyen Xuan Kiem <v.kiemnx@vinfast.vn>
2020-06-02 09:52:47 +0700

    Init project structer

---
+++ src/vn/plusplus/qna/Application.java
@@ -0,0 +1,7 @@
+package vn.plusplus.qna;
+
+public class Application {
+    public static void main(String[] args) {
+        // Start application from here
+    }
+}
---
+++ src/vn/plusplus/qna/model/Question.java
@@ -0,0 +1,4 @@
+package vn.plusplus.qna.model;
+
+public class Question {
+}
