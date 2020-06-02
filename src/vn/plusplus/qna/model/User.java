6ae18f4050e68e245b03ec0aa95b4be8ff82929d
Nhien <nguyenngocnhien98@gmail.com>
2020-06-02 20:37:07 +0700

    Model User

--- src/vn/plusplus/qna/model/User.java
+++ src/vn/plusplus/qna/model/User.java
@@ -1,4 +1,56 @@
 package vn.plusplus.qna.model;
 
 public class User {
+    String userName,fullName,email,phone;
+
+    public User() {
+    }
+
+    public User(String userName, String fullName, String email, String phone) {
+        this.userName = userName;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
 }
