482b5f649f4a2813085780d7c3b6d094c3334b62
Nhien <nguyenngocnhien98@gmail.com>
2020-06-03 22:04:54 +0700

    Task Answer

--- src/vn/plusplus/qna/model/Answer.java
+++ src/vn/plusplus/qna/model/Answer.java
@@ -1,4 +1,16 @@
 package vn.plusplus.qna.model;
 
+import java.util.List;
+
 public class Answer {
+    String userName;
+    List<AnswerItem> answerItems;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
