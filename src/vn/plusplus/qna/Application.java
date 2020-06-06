410dd413baa06830c63b57a4b639b8232da7f0d3
Nguyen Xuan Kiem <v.kiemnx@vinfast.vn>
2020-06-06 08:35:58 +0700

    Add main test

--- src/vn/plusplus/qna/Application.java
+++ src/vn/plusplus/qna/Application.java
@@ -1,18 +1,46 @@
 package vn.plusplus.qna;
 
 import vn.plusplus.qna.model.Answer;
-import vn.plusplus.qna.model.AnswerItem;
+import vn.plusplus.qna.model.Question;
 import vn.plusplus.qna.service.QNAService;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Xin moi lua chon chuc nang: ");
+        System.out.println("1. Kiem tra nang luc");
+        System.out.println("2. Xem lai bai thi");
+        System.out.println("3. Tim kiem thi sinh diem cao nhat");
+
+        int option = scanner.nextInt();
+        scanner.nextLine();
         QNAService qnaService = new QNAService();
-        List<AnswerItem> answerItems = new ArrayList<>();
-        answerItems.add(new AnswerItem("JV1_1", "A"));
-        Answer answer = new Answer("kiemnx", answerItems);
-        qnaService.saveAnswer(answer);
+
+        switch (option){
+            case 1:
+                System.out.println("Xin moi nhap vao language: ");
+                String language = scanner.nextLine();
+                System.out.println("Xin moi lua chon level: ");
+                String level = scanner.nextLine();
+                List<Question> questions = qnaService.findQuestionByCondition(language, level);
+                qnaService.displayQuestion(questions);
+                System.out.println("Nhap vao cau tra loi theo dinh dang: MA_CAU_HOI_1:CAU_TRA_LOI_1#MA_CAU_HOI_2:CAU_TRA_LOI_2");
+                String answerText = scanner.nextLine();
+                System.out.println("Nhap vao username:");
+                String userName = scanner.nextLine();
+                Answer answer = qnaService.buildAnswerFromUserInput(answerText, userName);
+                int result = qnaService.checkAnswer(questions, answer);
+                System.out.println("Ket qua cua ban la: " + result + "/" + questions.size() + " cau dung");
+                System.out.println("Ban co muon luu lai ket qua khong? Yes/No ");
+                String savedAnswer = scanner.nextLine();
+                if(savedAnswer.equals("Yes")){
+                    qnaService.saveAnswer(answer);
+                }
+        }
     }
+
+
 }
