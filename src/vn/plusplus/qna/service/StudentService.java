cbaa3956b6ca405681002950eb65eed55087fef2
nhiempc <nhiem10596@gmail.com>
2020-06-06 13:52:14 +0700

    Nhiem update Student Service

--- src/vn/plusplus/qna/service/StudentService.java
+++ src/vn/plusplus/qna/service/StudentService.java
@@ -2,7 +2,12 @@ package vn.plusplus.qna.service;
 
 import vn.plusplus.qna.interfaces.StudentInterface;
 import vn.plusplus.qna.model.Answer;
+import vn.plusplus.qna.model.Question;
 import vn.plusplus.qna.model.User;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentService implements StudentInterface {
 
@@ -12,8 +17,42 @@ public class StudentService implements StudentInterface {
     }
 
     @Override
-    public String findHighestScoreUserName() {
-        return null;
+    public String findHighestScoreUserName(String language, String level) {
+        FileReader fr = null;
+        BufferedReader br = null;
+        File file = new File("data/" + "answer_"+language+"_"+level+".text");
+        String filePath = file.getAbsolutePath();
+        String userHighestScore = "";
+        try {
+            fr = new FileReader(filePath);
+            br = new BufferedReader(fr);
+            String line = "";
+            List<String[]> answerList = new ArrayList<>();
+            while ((line = br.readLine())!=null){
+                String[] answerArray = line.split("#");
+                answerList.add(answerArray);
+            }
+            int highestScore = Integer.parseInt(answerList.get(0)[1]);
+            //answerArray example: nhiempc,80,JV1_1:A;JV1_2:B;....
+            for(int i = 0;i<answerList.size();i++){
+                if(Integer.parseInt(answerList.get(i)[1])>highestScore){
+                    highestScore = Integer.parseInt(answerList.get(i)[1]);
+                    userHighestScore = answerList.get(i)[0];
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Không tìm thấy dữ liệu!");
+        } catch (IOException e){
+            System.out.println("Lỗi đọc file!");
+        } finally {
+            try {
+                fr.close();
+                br.close();
+            } catch (Exception e){
+                System.out.println("Có lỗi xảy ra!");
+            }
+        }
+        return userHighestScore;
     }
 
     @Override
